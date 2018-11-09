f2c1667f164ca1fdb5bf2b89d0d2b2493f763154
LordNEXUS <Forgotten.NEXUS@Gmail.com>
2018-11-09 14:07:27 +0100

    DBHandler work: Added storeMatches method, is untested.

--- CompetenceMachineLearning/CompetenceMachineLearning/DBhandler.py
+++ CompetenceMachineLearning/CompetenceMachineLearning/DBhandler.py
@@ -3,7 +3,9 @@ from mysql.connector import errorcode
 import os
 import yaml
 from Competence import Competence
+from Advert import Advert 
 import random
+
 
 
 class DBHandler:
@@ -80,6 +82,42 @@ class DBHandler:
             
             return trainingAdverts, testingAdverts
 
+
+    def storeMatches(self, competenceID, advertIDs):
+        try:
+            cnx = mysql.connector.connect(**self.config)
+        except mysql.connector.Error as err:
+             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                 print("Something is wrong with your user name or password")
+             elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                 print("Database does not exist")
+             else:
+                 print(err)
+        else:
+            cursor = cnx.cursor()
+
+            # There is a limit on the values clause, only 1000 rows can be inserted at a time. Making a loop to generate multiple queries as needed.
+            # No need to check if the kompetence-annonce match exists already, the unique indexon the table should prevent duplicate rows from being added.
+            # A seperate method may be desired to clear all matches of a competence, but it's not this one.
+            i = 0
+            while i < len(advertIDs):
+                query = "insert into annonce_kompetence_machine(kompetence_id, annonce_id) values "
+                j = 0
+                done = False
+                while j < 950 && !done:
+                    if i + j < len(advertIDs):
+                        if j = 0:
+                            query += "(" + competenceID + ", " + advertIDs[i+j] + ")"
+                        else:
+                            query += ", (" + competenceID + ", " + advertIDs[i+j] + ")"
+                    else:
+                        done = True
+                    j += 1
+                cursor.execute(query)
+                i += 950
+                
+            cursor.close()
+            cnx.close()
 
     def retrive(self):
         try:
