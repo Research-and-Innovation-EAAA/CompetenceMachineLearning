f9172fa4ef4ad5c560c3160025d4f78a97fe3b4a
LordNEXUS <Forgotten.NEXUS@Gmail.com>
2018-11-14 10:54:27 +0100

    Syncing

--- CompetenceMachineLearning/CompetenceMachineLearning/DBhandler.py
+++ CompetenceMachineLearning/CompetenceMachineLearning/DBhandler.py
@@ -120,16 +120,8 @@ class DBHandler:
 
         cursor.execute("select name from machine_model where kompetence_id = " + str(competenceID) + " and name = '" + modelName + "'")
         if (len(list(cursor)) > 0):
-            cursor.close()
-            cnx.close()
-            cnx = self.createConnection()
-            cursor = cnx.cursor()
             cursor.execute("insert into machine_model(kompetence_id, model, weights, name) values(" + str(competenceID) + ", '" + modelJSON + "', '" + weightsJSON + "', '" + modelName + "')")
         else:
-            cursor.close()
-            cnx.close()
-            cnx = self.createConnection()
-            cursor = cnx.cursor()
             cursor.execute("update machine_model set model = '" + modelJSON + "', weights = '" + weightsJSON + "' where kompetence_id = " + str(competenceID) + " and name = '" + modelName + "'")
         cursor.close()
         cnx.close()
